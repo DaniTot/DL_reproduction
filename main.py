9edaabe36faa94fa412f971ab044450da6b2ddf7
Ruben van Oosterhoudt <r.c.vanoosterhoudt@student.tudelft.nl>
2021-03-10 10:52:18 +0100

    Added the code needed to extract High- and Low-level Features from the images

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello Ruben")
+import torchvision.models as models
+import torch.nn as nn
+
+resnet101 = models.resnet101(True, True)
+print(resnet101)
+ModelResHigh = nn.Sequential(*(list(resnet101.children())[:]))
+ModelResLow = nn.Sequential(*(list(resnet101.children())[:-2])) #2 isn't correct, look at questions.
+
+#featuresHigh = ModelResHigh('Input')
+#featuresLow = ModelResLow('Input')
