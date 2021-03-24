0113f6f2ec5c220cf0a752f1ba5bf6abce4fc628
DaniTot <tothdani1995@gmail.com>
2021-03-24 10:49:39 +0100

    Train class in main.py loads the video/frame, FeatureExtractor in FeatureExtractor.py passes it trough resNet101 for low and high lvl features.

---
+++ FeatureExtractor.py
@@ -0,0 +1,83 @@
+import torchvision.models as models
+from torchvision import transforms
+import torch.nn as nn
+import torch
+import numpy as np
+import cv2
+
+import os
+
+
+class FeatureExtractor:
+
+    def __init__(self):
+
+        self.frame = None
+
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225])])
+
+        self.model = None
+        self.HighFeatures = None
+        self.LowFeatures = None
+
+        self.new_frame = None
+        self.key_frame = None
+
+    def model_setup(self):
+        self.model = models.resnet101(pretrained=True)
+        self.HighFeatures = nn.Sequential(*(list(self.model.children())[:-2]))
+        self.LowFeatures = nn.Sequential(*(list(self.model.children())[0:6]))
+        self.HighFeatures.eval()
+        self.LowFeatures.eval()
+
+    def frame_setup(self):
+
+        if self.frame is None:
+            # Very first frame in the video/batch
+
+            img_old = self.transform(self.new_frame)
+            img_new = self.transform(self.new_frame)
+            img_nonkey = self.transform(self.new_frame)
+
+        else:
+            img_old, img_new, img_nonkey = torch.split(self.frame, 1, dim=0)
+            if self.key_frame is True:
+                # This is a key frame, so preserve the previous key and non-key frame
+                # Add the new frame
+                img_old = img_new
+                img_new = self.transform(self.new_frame)
+            else:
+                img_nonkey = self.transform(self.new_frame)
+
+        self.frame = torch.cat((torch.unsqueeze(img_old, 0),
+                                torch.unsqueeze(img_new, 0),
+                                torch.unsqueeze(img_nonkey, 0)), 0)
+
+        return self.frame
+
+    def new_vid(self):
+        self.frame = None
+
+    def get_feature(self, new_frame, key_frame):
+        ###
+        # lsts_rfcn/symbols/lsts_network_gaussian.py -> get_train_symbol_impression()
+        ###
+        self.new_frame = new_frame
+        self.key_frame = key_frame
+        self.frame_setup()
+
+        # HighFeatures takes old_key and new_key
+        high = self.HighFeatures(torch.cat(torch.split(self.frame, 1)[:2], 0))
+        conv_feat_oldkey, conv_feat_newkey = torch.split(high, 1)
+
+        # LowFeatures takes old_key, new_key, data_cur
+        feature_low_level = self.LowFeatures(torch.split(self.frame, 1)[2])
+
+        # Align old and new key features
+        # offset = mx.sym.Variable(name='init_offset', shape=(N, 2), lr_mult=10000*cfg.TRAIN.lr)
+
+        return conv_feat_oldkey, conv_feat_newkey, feature_low_level
--- main.py
+++ main.py
@@ -1,50 +1,34 @@
-import torchvision.models as models
-from torchvision import transforms
-import torch.nn as nn
-import torch
+import os
+import cv2
 import numpy as np
 
-import os
-from PIL import Image
+from FeatureExtractor import FeatureExtractor
+from LSTS import Agg_LSTS, Com_LSTS
 
 
 class Train:
-
     def __init__(self, batch_size):
-        ilscvrc_path = os.path.join(os.path.abspath(os.sep), 'Users', 'tothd', 'Documents', 'TU Delft', 'Msc',
-                                    'Deep Learning')
+        ilscvrc_path = os.path.join(os.path.abspath(os.sep), 'Users', 'tothd', 'Documents', 'TU Delft', 'Msc', 'Deep Learning')
         vid_path = os.path.join(ilscvrc_path, 'ILSVRC2015', 'Data', 'VID')
         self.train_set_path = os.path.join(vid_path, 'train', 'ILSVRC2015_VID_train_0001')
         assert os.path.isdir(self.train_set_path), self.train_set_path
 
         self.mp4_path = None
 
-        self.frame = None
-        self.data_cur = None
+        self.new_frame = None
+        self.new_scale = None
 
         self.batch_data_idx = None
-
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(
-                mean=[0.485, 0.456, 0.406],
-                std=[0.229, 0.224, 0.225])])
-
-        self.model = None
-        self.HighFeatures = None
-        self.LowFeatures = None
 
         self.batch_size = batch_size
         self.iteration_count = 0
 
         self.frame_count = 0
+        self.key_frame_interval = 10
 
-    def model_setup(self):
-        self.model = models.resnet101(pretrained=True)
-        self.HighFeatures = nn.Sequential(*(list(self.model.children())[:-2]))
-        self.LowFeatures = nn.Sequential(*(list(self.model.children())[0:6]))
-        self.HighFeatures.eval()
-        self.LowFeatures.eval()
+        self.SCALES = [600, 1000]  # first is scale (the shorter side); second is max size
+
+        self.FeEx = FeatureExtractor()
 
     def batch_randomizer(self):
         # count how many datasets we have
@@ -55,77 +39,82 @@ class Train:
 
         self.batch_data_idx = np.random.randint(0, mp4_count, self.batch_size)
 
+    def select_vid(self):
+        directory = os.fsencode(self.train_set_path)
+        mp4_lst = os.listdir(directory)
+        self.mp4_path = os.path.join(self.train_set_path,
+                                     os.fsdecode(mp4_lst[self.batch_data_idx[self.iteration_count]]))
+        # print(self.mp4_path)
+        return
+
     def load_frame(self):
         directory = os.fsencode(self.mp4_path)
         frame_lst = os.listdir(directory)
         frame_path = os.path.join(self.mp4_path, os.fsdecode(frame_lst[self.frame_count]))
 
         if os.path.isfile(frame_path):
-            if self.frame is None:
-                # Get the first two frames
-
-                img_new = self.transform(Image.open(frame_path))
-                self.frame = torch.unsqueeze(img_new, 0)
-                self.frame_count += 1
-
-                second_frame_path = os.path.join(self.mp4_path, os.fsdecode(frame_lst[self.frame_count]))
-                second_img_new = self.transform(Image.open(second_frame_path))
-
-                self.frame = torch.cat((self.frame, torch.unsqueeze(second_img_new, 0)), 0)
-
-            else:
-                # Preserve the previous frame
-                self.frame = torch.split(self.frame, 2, dim=0)[1]
-
-                # Add the new frame
-                img_new = self.transform(Image.open(frame_path))
-                self.frame = torch.cat((self.frame, torch.unsqueeze(img_new, 0)), 0)
+            # new_frame = Image.open(frame_path)
+            frame = cv2.imread(frame_path)
+            self.new_frame, self.new_scale = self.resize(frame, self.SCALES[0], self.SCALES[1], stride=0)
 
             self.frame_count += 1
             return True
+
         else:
             self.frame_count = 0
-            self.frame = None
             self.iteration_count += 1
             return False
 
-    def select_vid(self):
-        directory = os.fsencode(self.train_set_path)
-        mp4_lst = os.listdir(directory)
-        self.mp4_path = os.path.join(self.train_set_path, os.fsdecode(mp4_lst[self.batch_data_idx[self.iteration_count]]))
+    @staticmethod
+    def resize(im, target_size, max_size, stride=0, interpolation=cv2.INTER_LINEAR):
+        """
+        only resize input image to target size and return scale
+        :param im: BGR image input by opencv
+        :param target_size: one dimensional size (the short side)
+        :param max_size: one dimensional max size (the long side)
+        :param stride: if given, pad the image to designated stride
+        :param interpolation: if given, using given interpolation method to resize image
+        :return:
+        """
+        im_shape = im.shape
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+        im_scale = float(target_size) / float(im_size_min)
+        # prevent bigger axis from being more than max_size:
+        if np.round(im_scale * im_size_max) > max_size:
+            im_scale = float(max_size) / float(im_size_max)
+        im = cv2.resize(im, None, None, fx=im_scale, fy=im_scale, interpolation=interpolation)
 
-        return
+        if stride == 0:
+            return im, im_scale
+        else:
+            # pad to product of stride
+            im_height = int(np.ceil(im.shape[0] / float(stride)) * stride)
+            im_width = int(np.ceil(im.shape[1] / float(stride)) * stride)
+            im_channel = im.shape[2]
+            padded_im = np.zeros((im_height, im_width, im_channel))
+            padded_im[:im.shape[0], :im.shape[1], :] = im
+            return padded_im, im_scale
 
-    def run(self):
+    def get_feature(self):
         # Setup
-        self.model_setup()
         self.batch_randomizer()
         for iteration in range(self.batch_size):
             self.select_vid()
             while True:
                 if self.load_frame():
-                    # The image is loaded here
 
-                    ###
-                    # lsts_rfcn/symbols/lsts_network_gaussian.py -> get_train_symbol_impression()
-                    ###
+                    # Get thefeature vectors from the new frame
+                    if self.iteration_count % self.key_frame_interval == 0:
+                        key_frame = True
+                    else:
+                        key_frame = False
 
-                    # HighFeatures takes old_key and new_key
-                    high = self.HighFeatures(self.frame)
-                    features = torch.split(high, 1)
+                    conv_feat_oldkey, conv_feat_newkey, feature_low_level = self.FeEx.get_feature(self.new_frame, key_frame)
 
-                    conv_feat_oldkey = features[0]
-                    conv_feat_newkey = features[1]
+                    # TODO: align conv_feat_oldkey to conv_feat_newkey
 
-                    print(conv_feat_newkey)
-
-                    # LowFeatures takes old_key, new_key, data_cur
-                    # TODO: Get data_cur somehow
-                    low = self.LowFeatures(torch.cat((self.frame, self.data_cur), 0))
-                    features_low_level_slice = torch.split(low, 1)
-
-
-                    #TODO: Align conv_feat_oldkey and conv_feat_newkey
+                    # TODO:
 
                     #TODO: key2key quality network
 
@@ -134,7 +123,3 @@ class Train:
                     return
                 else:
                     break
-
-
-Tr = Train(1)
-Tr.run()
