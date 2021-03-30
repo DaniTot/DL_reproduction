959b24321c04e215a0dd4ef08ec3c4976d4aaab7
Ruben van Oosterhoudt <r.c.vanoosterhoudt@student.tudelft.nl>
2021-03-30 10:39:15 +0200

    Added the quality network

--- LSTS.py
+++ LSTS.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 import numpy as np
 
 class Com_LSTS():
@@ -14,12 +15,31 @@ class Com_LSTS():
         self.s_pn = None
         self.S_pn = None
 
+        self.quality = torch.nn.Sequential(
+            torch.nn.Conv2d(in_channels = 1024, out_channels =256, kernel_size = (1,1)),
+            torch.nn.Conv2d(in_channels = 256, out_channels= 16, kernel_size= (3,3), padding=(1,1)),
+            torch.nn.Conv2d(in_channels = 16, out_channels= 1, kernel_size= (3,3), padding=(1,1))
+        )
+
         print('Initialization done!')
 
     def get_input(self, old_key, new_key):
         # TODO: which one is the old key, and which one is the noew key, and where does the non key frame go?
         self.F_t = None
         self.F_tk = None
+
+    def quality_network(self, feat1, feat2):
+        feature_concat = torch.cat([feat1, feat2], dim=0)
+        quality_res = self.quality(feature_concat)
+        quality_weights = torch.split(quality_res, quality_res.shape[0]/2, dim=0)
+        quality_weights = F.softmax(torch.cat([quality_weights[0], quality_weights[1]], dim=1))
+        quality_weights_splice = torch.split(quality_weights, quality_weights.shape[0]/2, dim=0)
+
+        quality_weight1 = torch.tile(quality_weights_splice[0], (1, 1024, 1, 1))
+        quality_weight2 = torch.tile(quality_weights_splice[1], (1, 1024, 1, 1))
+
+        feat_result = torch.add(quality_weight1*feat1, quality_weight2*feat2)
+        return feat_result
 
     def sample_offset(self, N, K, C, H, W, lr):
         ###
@@ -130,13 +150,13 @@ class Agg_LSTS():
         self.pn = torch.ones([In_Size, In_Size])
         self.F_tk = torch.ones([In_Size, In_Size])
 
-    def Aggregation(self):
+    def Aggregation(self, S_pn):
         for i in range(self.In_Size):
             for j in range(self.In_Size):
-                self.F_tk = Compare.S_pn[i, j]*self.Ft[i, j]
+                self.F_tk = S_pn[i, j]*self.Ft[i, j]
 
-    def Updating(self):
-        self.F_tk = self.Aggregation()
+    def Updating(self, S_pn):
+        self.F_tk = self.Aggregation(S_pn)
         print("Updating")
         return(self.F_tk)
 
@@ -144,7 +164,8 @@ class Agg_LSTS():
 In_Size = 20
 Compare = Com_LSTS(In_Size)
 G = Compare.Run()
-#Aggreg = Agg_LSTS(In_Size)
-#diff = Aggreg.Updating()
+print(G)
+Aggreg = Agg_LSTS(In_Size)
+diff = Aggreg.Updating(G)
 
 print(diff)
